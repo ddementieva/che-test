3b51485efa9f96bef3ad946e0d2e24fa46ac0417
Daria Dementieva <ddementieva@codenvy.com>
2016-07-14 12:37:29 +0300

    Initial commit

---
+++ src/main/java/org/zsomb/dodoma/Application.java
@@ -0,0 +1,10 @@
+package org.zsomb.dodoma;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello "+SpringApplication.class.getName());
+    }
+}
